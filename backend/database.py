4fee1ce88065932c77729817160b2607efd3523b
GIRIDHARAN <giridharan172006@gmail.com>
2026-02-10 10:57:30 +0530

    adding gitginore

--- backend/database.py
+++
@@ -1,6 +0,0 @@
-# db.py
-from pymongo import MongoClient
-MONGO_URI = "mongodb://localhost:27017/"  # or Atlas URI
-client = MongoClient(MONGO_URI)
-db = client["vitelora"]
-users_collection = db["users"]
